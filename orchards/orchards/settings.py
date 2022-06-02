9ff6c1e81f510b28c31250170ae5689efe412827
kabz twala <kabz.twala@gmail.com>
2022-06-02 18:32:35 +0200

    set debug to false

--- orchards/orchards/settings.py
+++ orchards/orchards/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_v_!+#2tqjnbbtm3vb)0fj#%hql=g88nlnd)%50(wg-7(@ru-^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
